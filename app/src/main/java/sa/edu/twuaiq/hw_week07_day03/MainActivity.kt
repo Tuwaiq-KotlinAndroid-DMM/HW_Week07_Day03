bd783f10eba8b1cc7a4f176e356fe4adcbd4a307
MohamedFouad <mohamed.a.fouad@outlook.com>
2021-11-07 21:15:22 +0300

    Initial commit

---
+++ app/src/main/java/sa/edu/twuaiq/hw_week07_day03/MainActivity.kt
@@ -0,0 +1,11 @@
+package sa.edu.twuaiq.hw_week07_day03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
