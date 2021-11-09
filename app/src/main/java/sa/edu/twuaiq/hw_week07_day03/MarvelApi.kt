b816158849a89dc994e6cabec5c1485f81a12337
HalaAlbaqawi <hala.hammed.dmm.k1@tuwaiq.edu.sa>
2021-11-09 20:26:58 +0300

    HW

---
+++ app/src/main/java/sa/edu/twuaiq/hw_week07_day03/MarvelApi.kt
@@ -0,0 +1,12 @@
+package sa.edu.twuaiq.hw_week07_day03
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApi {
+
+     @GET("/demos/marvel/")
+     fun getListMarvel (@Query("/demos/marvel/") demos: Int): Call<List<MarvelModel>>
+
+}
